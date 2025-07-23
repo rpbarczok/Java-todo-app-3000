b48100115459de760ae44ba7894190973d137ba0
AccessCoder <info.unaddict@gmail.com>
2025-07-23 09:12:08 +0200

    Initial commit

---
+++ src/main/java/org/example/javatodoapp3000/JavaTodoApp3000Application.java
@@ -0,0 +1,13 @@
+package org.example.javatodoapp3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoApp3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodoApp3000Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javatodoapp3000/JavaTodoApp3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javatodoapp3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTodoApp3000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
